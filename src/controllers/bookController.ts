36de47f34f72ec320945a0297679257e9b54f177
Daniel Marques <danielmarques7@hotmail.com>
2021-06-01 23:11:34 -0300

    Create 'create book' endpoint

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -12,3 +12,15 @@ export const get = async (req: Request, res: Response) => {
     return res.status(500).send({ error });
   }
 };
+
+export const create = async (req: Request, res: Response) => {
+  try {
+    const { title, description } = req.body;
+
+    const book = await Book.create({ title, description });
+
+    return res.status(201).send(book);
+  } catch (error) {
+    return res.status(400).send({ error });
+  }
+};
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -4,4 +4,5 @@ import * as bookController from '../controllers/bookController';
 const router: Router = express.Router();
 
 export default router
-  .get('/:id', bookController.get);
+  .get('/:id', bookController.get)
+  .post('/', bookController.create);
