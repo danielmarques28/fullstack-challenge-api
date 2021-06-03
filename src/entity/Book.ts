55d1cceb19a2197498f43faac751a23df9f5a5ea
Daniel Marques <danielmarques7@hotmail.com>
2021-06-03 15:59:00 -0300

    Create Book entity

---
+++ src/entity/Book.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Author } from './Author';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column('text')
+  description!: string;
+}
